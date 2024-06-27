cef4100de5835d8e149062b402053c2dc6d10dd1
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-06-27 15:29:43 +0300

    feat: project structure

---
+++ anchoridl.go
@@ -0,0 +1,7 @@
+package anchor_idl
+
+import "errors"
+
+func ParseArgs(data []byte, idlJson string) (map[string]interface{}, error) {
+	return nil, errors.New("idk what happened")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/BCH-labs/anchor-idl-go
+
+go 1.22.4
